ec48e8d5bd0511944e0868fb468da9c9647a3588
juheun park <juhen00@gmail.com>
2026-06-02 20:48:19 +0900

    Initial Commit

---
+++ Manifestly/ManifestlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManifestlyApp.swift
+//  Manifestly
+//
+//  Created by margarine on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ManifestlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
